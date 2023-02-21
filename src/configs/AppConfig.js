38a62e3f596fd55626795656a5b96ced7f3f51b5
creatosolution <info@creatosolution.com>
2023-02-21 13:57:13 +0530

    SSL end point added

--- src/configs/AppConfig.js
+++ src/configs/AppConfig.js
@@ -2,7 +2,7 @@ import { SIDE_NAV_LIGHT, NAV_TYPE_SIDE, DIR_LTR } from 'constants/ThemeConstant'
 import { env } from './EnvironmentConfig'
 
 export const APP_NAME = 'Stock Fish';
-export const API_BASE_URL = "http://api.stockmeta.in/api"
+export const API_BASE_URL = "https://api.stockmeta.in/api"
 export const APP_PREFIX_PATH = '/app';
 export const AUTH_PREFIX_PATH = '/auth';
 export const REDIRECT_URL_KEY = 'redirect'
